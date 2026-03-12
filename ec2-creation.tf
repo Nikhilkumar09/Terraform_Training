65fac83ef2df818b68d4286f6fdccd9c0a65685a
Nikhilkumar09 <knikhil095@gmail.com>
2026-03-13 03:34:41 +0530

    tf files commit

---
+++ ec2-creation.tf
@@ -0,0 +1,40 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+
+
+
+variable "environment" {
+  default = "development"
+}
+
+resource "aws_instance" "nikhil" {
+    ami = "ami-076c6dbba59aa92e6"
+    instance_type = var.environment == "development" ? "t2.micro" : "error"
+    key_name      = "TEST_EC2_INST_2_KEY" 
+    vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+    tags = {
+    Name = "TEST_EC2_INST_nikhil"
+    }
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-076c6dbba59aa92e6"
+    instance_type = "t2.micro"
+    key_name      = "TEST_EC2_INST_2_KEY"           # Replace with your actual key pair name
+
+
+tags = {
+    Name = "TEST_EC2_INST"
+}
+
+}
