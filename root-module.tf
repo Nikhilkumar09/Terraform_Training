53167c1907c697544a2664887e3d4f44fac30b2b
Nikhilkumar09 <66530687+Nikhilkumar09@users.noreply.github.com>
2026-03-13 21:03:14 -0400

    Update root-module.tf

--- root-module.tf
+++ root-module.tf
@@ -1,10 +1,10 @@
-module "PRVMODULE" {
-  source  = "app.terraform.io/FALCON_ORG/PRVMODULE/random"
-  version = "1.0.1"
+module "STAGING" {
+  source  = "app.terraform.io/FALCON_ORG/TEST/random"
+  version = "1.0.0"
 }
 
 output "result" {
-  value = module.PRVMODULE.value
+  value = module.STAGING.value
 }
 
 
